242b8283e2dcc0fc8361e84f8f670d1e5c8742e6
isahnoune <31284645+isahnoune@users.noreply.github.com>
2017-09-14 12:38:46 -0500

    Add files via upload

--- inclass5.m
+++ inclass5.m
@@ -9,7 +9,62 @@
 % of black/white pixels optional with a default value of 1% if not
 % specified. 
 
+%a
+img = imread('Cat.jpg');
+a = function1(img);
+
+function image = function1(img)
+stretchlim(img);
+image = imadjust(img, stretchlim(img), [0.1, 0.99]);
+imshow(image);
+end
+
+%b
+number = 0.3;
+img = imread('Cat.jpg');
+a = function2(img,number);
+
+function image = function2(img,number)
+stretchlim(img);
+image = imadjust(img, stretchlim(img), [number, 1-number]);
+imshow(image);
+end
+
+%c
+number = 1;
+img = imread('Cat.jpg');
+a = function3(img, number);
+
+function image = function3(img,number)
+if nargin == 2
+    z = stretchlim(img);
+    image = imadjust(img, stretchlim(img), [number, 1-number]);
+elseif nargin == 1
+    image = imadjust(img, stretchlim(img), [0.1, 0.99]);
+end
+imshow(image);
+end
+
 %2. Write a function to take the reverse complement of a DNA sequence, that
 %is, returns the complementary base pair for each base, read from end
 %to beginning. (e.g. the reverse compliment of ATGC is GCAT). Do not use the
 %builtin MATLAB function for this. 
+
+DNA = 'ACTGCTAGCTA';
+DNA2 = function4(DNA);
+
+function ReverseDNA = function4(DNA)
+DNA2 = "";
+DNA = reverse(DNA);
+for i = 1:length(DNA)
+if DNA(i) == 'A'
+    DNA2 = DNA2 + 'T';
+elseif DNA(i) == 'G'
+    DNA2 = DNA2 + 'C';
+elseif DNA(i) == 'C'
+    DNA2 = DNA2 + 'G';
+elseif DNA(i) == 'T'
+    DNA2 = DNA2 + 'A';
+end
+ReverseDNA = DNA2;
+end
